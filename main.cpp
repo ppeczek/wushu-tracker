4514cb6e77bb01f2b882242ea2decc21e4ad21b6
Piotr Peczek <ppeczek@gmail.com>
2018-02-15 16:40:40 +0100

    AWS S3

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 //
 // Created by ppeczek on 5/12/17.
 //
+#include <aws/core/Aws.h>
+#include <aws/s3/S3Client.h>
+#include <aws/s3/model/GetObjectRequest.h>
+#include <fstream>
+
 #include <boost/program_options.hpp>
 #include "VideoAnalyzer.cpp"
 
@@ -16,9 +21,15 @@ ostream& operator<<(ostream& os, const vector<T>& v)
     return os;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
+    // 1-1-1[1].mp4 --corner-AX=-90 --corner-AY=565 --corner-BX=788 --corner-BY=460 --corner-CX=1310 --corner-CY=480 --corner-DX=1010 --corner-DY=725
     // -std=c++11 `pkg-config opencv --cflags --libs`
     // https://s3.eu-central-1.amazonaws.com/wushu-videos/media/0001%5B1%5D.mp4
+    Aws::String bucket_name = "wushu-videos";
+    Aws::String key_name;
+
+    Aws::SDKOptions options;
+    Aws::InitAPI(options);
 
     std::vector<std::string> inputFiles;
     Point cornerA = Commons::nullPoint,
@@ -65,37 +76,64 @@ int main(int argc, char *argv[]) {
             cout << "Input files are: " << inputFiles << "\n";
         }
 
-        clock_t start = clock();
-        std::vector<VideoAnalyzer> analyzers;
+//        for (auto& file: inputFiles) {
+            Aws::Client::ClientConfiguration config;
+            config.region = "eu-central-1";
+            Aws::S3::S3Client s3_client(config);
 
-        for (auto& file: inputFiles) {
-            analyzers.emplace_back(VideoAnalyzer(AnalyzerSettings(file), {cornerA, cornerB, cornerC, cornerD}));
-        }
+//            key_name = "media/" + file;
 
-        int i = 0;
-        for (auto&& va : analyzers) {
-            cout << "Video " << ++i << endl;
-            va.analyze(va.PPECZEK);
-//        va.analyze(va.MIL);
-//        va.analyze(va.BOOSTING);
-//        va.analyze(va.KCF);
-//        va.analyze(va.TLD);
-//        va.analyze(va.MEDIANFLOW);
-//        va.analyze(va.GOTURN);
-            cout << va.getStatistics() << endl;
-            cout << endl;
+            Aws::S3::Model::GetObjectRequest object_request;
+            object_request.WithBucket(bucket_name).WithKey(key_name);
 
-            va.drawPaths();
-        }
+            auto get_object_outcome = s3_client.GetObject(object_request);
 
-        double duration = (clock() - start) / (double) CLOCKS_PER_SEC;
-        cout << "Program duration: " << duration << endl;
+            if (get_object_outcome.IsSuccess()) {
+                Aws::OFStream local_file;
+                local_file.open(key_name.c_str(), std::ios::out | std::ios::binary);
+                local_file << get_object_outcome.GetResult().GetBody().rdbuf();
+                std::cout << "Done!" << std::endl;
+            } else {
+                std::cout << "GetObject error: " <<
+                          get_object_outcome.GetError().GetExceptionName() << " " <<
+                          get_object_outcome.GetError().GetMessage() << std::endl;
+            }
+//        }
+
+//        clock_t start = clock();
+//        std::vector<VideoAnalyzer> analyzers;
+//
+//        for (auto& file: inputFiles) {
+//            analyzers.emplace_back(VideoAnalyzer(AnalyzerSettings(file), {cornerA, cornerB, cornerC, cornerD}));
+//        }
+//
+//        int i = 0;
+//        for (auto&& va : analyzers) {
+//            cout << "Video " << ++i << endl;
+//            va.analyze(va.PPECZEK);
+////        va.analyze(va.MIL);
+////        va.analyze(va.BOOSTING);
+////        va.analyze(va.KCF);
+////        va.analyze(va.TLD);
+////        va.analyze(va.MEDIANFLOW);
+////        va.analyze(va.GOTURN);
+//            cout << va.getStatistics() << endl;
+//            cout << endl;
+//
+//            va.drawPaths();
+//        }
+//
+//        double duration = (clock() - start) / (double) CLOCKS_PER_SEC;
+//        cout << "Program duration: " << duration << endl;
+
     }
     catch(exception& e)
     {
         cout << e.what() << "\n";
         return 1;
     }
+
+    Aws::ShutdownAPI(options);
     return 0;
 }
 
